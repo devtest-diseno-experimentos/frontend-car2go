6c38c6f4aa99016309862394b50e16964ec4ddec
Jean Franco <u202219284@upc.edu.pe>
2024-09-23 05:09:32 -0500

    feat(authentication): updated toolbar view

--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+  isScrolled = false;
+  isMenuOpen = false;
+  userRole: string = '';
 
+  ngOnInit() {
+
+    this.userRole = localStorage.getItem('userRole') || '';
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset;
+    this.isScrolled = offset > 50;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks) {
+      if (this.isMenuOpen) {
+        navLinks.classList.add('active');
+      } else {
+        navLinks.classList.remove('active');
+      }
+    }
+  }
 }
