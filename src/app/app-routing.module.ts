08e6a55f78d429acbaccbe50bdc297352a16c0b0
Jean Franco <u202219284@upc.edu.pe>
2024-09-23 17:12:14 -0500

    feat(authentication): added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/pages/home/home.component';
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {FooterComponent} from "./public/components/footer/footer.component";
 import {LoginComponent} from "./register/components/login/login.component";
 import {RegisterComponent} from "./register/components/register/register.component";
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: 'footer', component:FooterComponent },
   { path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
-  {path: 'forgot-password', component: ForgotPasswordComponent }
+  {path: 'forgot-password', component: ForgotPasswordComponent },
+  {path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
