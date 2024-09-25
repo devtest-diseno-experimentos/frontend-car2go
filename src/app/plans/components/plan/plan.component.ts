a3ed5f867337344ca86725f083702152802a12e1
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-09-25 01:48:24 -0500

    feat(payment): updated plan component.

--- src/app/plans/components/plan/plan.component.ts
+++ src/app/plans/components/plan/plan.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
-  styleUrl: './plan.component.css'
+  styleUrls: ['./plan.component.css']
 })
 export class PlanComponent {
+  constructor(private router: Router) {}
 
+  redirectToPaymentForm() {
+    this.router.navigate(['/payment-form']);
+  }
 }
