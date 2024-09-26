1f6b8228058fbed991d71ad8e673a00ea82e2f3e
Joaquin Carranza <joaquincarranza1234@gmail.com>
2024-09-25 21:58:45 -0500

    feat(interaction): mechanic_component

---
+++ src/app/mechanic_review/mechanic-check/mechanic-check.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mechanic-check',
+  templateUrl: './mechanic-check.component.html',
+  styleUrl: './mechanic-check.component.css'
+})
+export class MechanicCheckComponent {
+
+}
