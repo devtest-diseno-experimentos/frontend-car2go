cde6a2f26a76f61e0d0f43e67e00819a5c799481
Jean Franco <u202219284@upc.edu.pe>
2024-09-26 22:24:06 -0500

    feat(authentication): updated home component

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import {CarService} from "../../../cars/services/car.service";
+import { CarService } from "../../../cars/services/car.service";
+import {FavoriteService} from "../../../cars/services/favorite-service/favorite.service";
 
 @Component({
   selector: 'app-home',
@@ -11,28 +12,42 @@ export class HomeComponent implements OnInit {
   userRole: string = '';
   showForm: boolean = false;
   cars: any[] = [];
+  defaultImage: string = 'assets/default_image.jpg';
 
-  constructor(private router: Router, private carService: CarService) {}
+  constructor(private router: Router, private carService: CarService, private favoriteService: FavoriteService) {}
 
   ngOnInit() {
     this.userRole = localStorage.getItem('userRole') || '';
-    this.cars = this.carService.getCars();
+    this.carService.getCars().subscribe(
+      (data: any[]) => {
+        this.cars = data.map(car => {
+          car.image = car.image || this.defaultImage;
+          car.images = car.images && car.images.length > 0 ? car.images : [this.defaultImage];
+          return car;
+        });
+      },
+      (error) => {
+        console.error('Error al obtener los autos:', error);
+      }
+    );
   }
 
-  openModal() {
-    this.showForm = true;
+  navigateToCarListingForm() {
+    this.router.navigate(['/car-listing-form']);
   }
-
-  closeModal() {
-    this.showForm = false;
-  }
-
 
   viewCarDetails(carId: number) {
     this.router.navigate(['/car-details', carId]);
   }
 
-  onFormClosed() {
-    this.showForm = false;
+  addToFavorites(carId: number): void {
+    this.favoriteService.addFavorite(carId).subscribe(
+      response => {
+        console.log('Car added to favorites:', response);
+      },
+      error => {
+        console.error('Error adding car to favorites:', error);
+      }
+    );
   }
 }
