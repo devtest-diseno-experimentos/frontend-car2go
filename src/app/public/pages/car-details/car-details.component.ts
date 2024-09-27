e06195eb172a1c7b2022d34c26ca3084c2517dfa
Jean Franco <u202219284@upc.edu.pe>
2024-09-26 22:25:19 -0500

    feat(authentication): updated footer car details component

--- src/app/public/pages/car-details/car-details.component.ts
+++ src/app/public/pages/car-details/car-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {CarService} from "../../../cars/services/car.service";
+import { CarService } from '../../../cars/services/car.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car-details',
@@ -10,12 +11,101 @@ import {CarService} from "../../../cars/services/car.service";
 export class CarDetailsComponent implements OnInit {
   car: any;
   userRole: string = '';
+  mainImage: string = '';
+  images: string[] = [];
+  defaultImage: string = 'assets/default_image.jpg';
+  carForm: FormGroup;
+  isModalOpen: boolean = false;
 
-  constructor(private route: ActivatedRoute, private carService: CarService) {} // Inyecta el servicio
+  constructor(
+    private route: ActivatedRoute,
+    private carService: CarService,
+    private fb: FormBuilder
+  ) {
+    this.carForm = this.fb.group({
+      title: ['', Validators.required],
+      name: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      brand: ['', Validators.required],
+      model: ['', Validators.required],
+      color: ['', Validators.required],
+      year: ['', [Validators.required, Validators.pattern(/^[0-9]{4}$/)]],
+      price: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      transmission: ['', Validators.required],
+      engine: ['', Validators.required],
+      mileage: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      doors: ['', Validators.required],
+      plate: ['', Validators.required],
+      location: ['', Validators.required],
+      description: ['', Validators.required],
+      image: [''],
+      fuel: ['', Validators.required],
+      speed: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      images: [[]]
+    });
+  }
 
   ngOnInit() {
-    const carId = +this.route.snapshot.paramMap.get('id')!;
-    this.car = this.carService.getCarById(carId);
+    this.route.paramMap.subscribe(params => {
+      const carId = +params.get('id')!;
+      this.carService.getCarById(carId).subscribe(
+        (data: any) => {
+          this.car = data;
+          this.updateImages();
+          this.populateForm();
+        }
+      );
+    });
+
     this.userRole = localStorage.getItem('userRole') || '';
+  }
+
+  updateImages() {
+    if (this.car) {
+      this.mainImage = this.car.mainImage || this.car.image || this.defaultImage;
+      this.images = this.car.images && this.car.images.length > 0 ? this.car.images : [this.defaultImage];
+    } else {
+      this.mainImage = this.defaultImage;
+      this.images = [this.defaultImage];
+    }
+  }
+
+  changeImage(image: string) {
+    this.mainImage = image;
+  }
+
+  openEditModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeEditModal(): void {
+    this.isModalOpen = false;
+  }
+
+  populateForm() {
+    if (this.car) {
+      this.carForm.patchValue(this.car);
+      this.carForm.get('images')?.setValue(this.car.images || []);
+    }
+  }
+
+  editCar() {
+    if (this.carForm.valid) {
+      const updatedCar = {
+        ...this.carForm.value,
+        images: this.images
+      };
+      this.carService.updateCar(this.car.id, updatedCar).subscribe(
+        (response) => {
+          this.car = response;
+          this.closeEditModal();
+          console.log('Coche actualizado:', response);
+        },
+        (error) => {
+          console.error('Error al actualizar el coche:', error);
+        }
+      );
+    }
   }
 }
