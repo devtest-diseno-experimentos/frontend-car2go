30647561d880a20ac293d33168caa5e5f213d2b7
Jean Franco <u202219284@upc.edu.pe>
2024-09-26 22:33:11 -0500

    feat(authentication): added profile component

---
+++ src/app/profile/profile/profile.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../register/service/auth.service";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user: any;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const userId = +localStorage.getItem('id')!;
+    this.authService.getUserInfo(userId).subscribe(
+      (data: any) => {
+        this.user = data;
+      },
+      (error) => {
+        console.error('Error al obtener la información del usuario:', error);
+      }
+    );
+  }
+}
