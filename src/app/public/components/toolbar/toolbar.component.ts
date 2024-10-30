bdba82261ee755be0009d7ca199a211e5abfaa4a
Jean Franco <u202219284@upc.edu.pe>
2024-10-30 01:09:26 -0500

    feat(interaction): updated toolbar component

--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -1,18 +1,24 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from '../../../register/service/auth.service';
 import { Router } from '@angular/router';
+import {ProfileService} from "../../../profiles/services/profile.service";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+  styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
   isScrolled = false;
   isMenuOpen = false;
   userRole: string = '';
+  userPhoto: string | null = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private profileService: ProfileService
+  ) {}
 
   logout(): void {
     this.authService.logout();
@@ -20,8 +26,23 @@ export class ToolbarComponent implements OnInit {
   }
 
   ngOnInit() {
-
     this.userRole = localStorage.getItem('userRole') || '';
+
+    const userId = +localStorage.getItem('id')!;
+    if (userId) {
+      this.profileService.getProfileByUserId(userId).subscribe(
+        (profile) => {
+          if (profile && profile.length > 0) {
+            this.userPhoto = profile[0].photo;
+          } else if (profile && profile.userId === userId) {
+            this.userPhoto = profile.photo;
+          }
+        },
+        (error) => {
+          console.error('Error al obtener el perfil del usuario:', error);
+        }
+      );
+    }
   }
 
   @HostListener('window:scroll', [])
